1156388dbd33528de93958927b1f95dcdf0b0402
Tracyaa <tracyho0867@gmail.com>
2019-04-15 17:15:45 -0400

    change keyword fetch link

--- src/adapters/adapter.js
+++ src/adapters/adapter.js
@@ -1,4 +1,4 @@
-const KEYWORD_BASE_URL = 'https://dry-shelf-10302.herokuapp.com/api/v1/keywords/1'
+const KEYWORD_BASE_URL = 'https://dry-shelf-10302.herokuapp.com/api/v1/keywords/2'
 // const BASE_URL = "http://dry-shelf-10302.herokuapp.com/api/v1/ideas"
 const TOOL_BASE_URL = 'https://dry-shelf-10302.herokuapp.com/api/v1/tools'
 const IDEA_BASE_URL = 'https://dry-shelf-10302.herokuapp.com/api/v1/ideas'
