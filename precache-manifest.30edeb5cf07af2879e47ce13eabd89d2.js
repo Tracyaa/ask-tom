17f3e9a6801718858dba3b952edcb9af308ffb2d
Angelo Spampinato <spampinatodesign@gmail.com>
2019-04-17 16:17:17 -0400

    Updates

---
+++ precache-manifest.30edeb5cf07af2879e47ce13eabd89d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66f6807fa703cfaa1a68",
+    "url": "/ask-tom/static/css/main.8c922d40.chunk.css"
+  },
+  {
+    "revision": "66f6807fa703cfaa1a68",
+    "url": "/ask-tom/static/js/main.79c8d6e4.chunk.js"
+  },
+  {
+    "revision": "23fb081a3df0a61baace",
+    "url": "/ask-tom/static/js/runtime~main.bc2ac049.js"
+  },
+  {
+    "revision": "454e0910d9b0212f974a",
+    "url": "/ask-tom/static/js/2.46c00421.chunk.js"
+  },
+  {
+    "revision": "56751f2e013e902432d173cc461d5e3a",
+    "url": "/ask-tom/index.html"
+  }
+];
